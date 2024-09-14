7b15bb3f5a20271a6d27fd96144b049601130456
Mohan1github <jmohankumar62@gmail.com>
2024-09-14 09:29:07 +0530

    ayuthor delete added

--- server/routes/authorroutes.js
+++ server/routes/authorroutes.js
@@ -1,10 +1,11 @@
 const express = require("express")
-const { getallauthor, singleauthor, createauthor } = require("../controllers/authorcontroller")
+const { getallauthor, singleauthor, createauthor,deleteauthor } = require("../controllers/authorcontroller")
 
 const authorrouter = express.Router
 
 authorrouter.get("/all-author", getallauthor)
 authorrouter.get("/get-author/:id", singleauthor)
 authorrouter.get("/create-author", createauthor)
+authorrouter.delete("/delete-author/:id",deleteauhtor)
 
 module.exports = {authorrouter}
